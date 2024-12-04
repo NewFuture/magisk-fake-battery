33498a44a7fb8aea39b420c4b7cb9dc5d65181ef
Hieu Dang <xuanhieu1989@gmail.com>
2024-12-04 15:19:25 +0700

    wait boot complete

--- service.sh
+++ service.sh
@@ -1,4 +1,8 @@
 #!/system/bin/sh
 # wait boot complete and battery service ready
-sleep 5  # custom time if needed
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+    sleep 1
+done
+sleep 5
+echo "System boot complete. Proceeding with fake battery..."
 dumpsys battery set level 100
